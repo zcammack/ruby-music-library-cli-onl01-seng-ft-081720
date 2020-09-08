95ea73cfb29fa543ec031afe440bb7af21bd31d8
Zackary Cammack <zack.cammack@gmail.com>
2020-09-08 17:11:40 +0000

    Done.

---
+++ lib/Song.rb
@@ -0,0 +1,74 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
+    @name = name
+    self.artist = (artist) if artist.class == Artist
+    self.genre = (genre) if genre.class == Genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre
+    @genre
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+      song.name == name
+    end
+  end
+
+  def self.create(name)
+    self.new(name).tap do |song|
+      song.save
+    end
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ||
+    self.create(name)
+  end
+
+  def self.new_from_filename(name)
+    song_name = name.split(" - ")[1]
+    artist_name = name.split(" - ")[0]
+    genre_name = name.split(" - ")[2]
+    .chomp(".mp3")
+    song = Song.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.genre = Genre.find_or_create_by_name(genre_name)
+    song
+  end
+
+  def self.create_from_filename(file)
+    self.new_from_filename(file).save
+  end
+end
