a9811c37ebfc75635dff70067156b8652ba70c2b
Zackary Cammack <zack.cammack@gmail.com>
2020-09-08 16:58:53 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -95,6 +95,7 @@ class MusicLibraryController
       list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
 
       input = gets.strip.to_i
+      binding.pry
       if (1..Song.all.length).include?(input)
         song = list_of_songs[input+2]
         puts "Playing #{song.name} by #{song.artist.name}"
