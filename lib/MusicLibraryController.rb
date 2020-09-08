64b43c1ee23b22cc81e99ec207589316a2a52e4e
Zackary Cammack <zack.cammack@gmail.com>
2020-09-08 17:09:22 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -92,11 +92,8 @@ class MusicLibraryController
 
     def play_song
       puts "Which song number would you like to play?"
-
       input = gets.strip
-
-      if (1..Song.all.length).include?(input)
-        song = list_of_songs[input+2]
+      if input.to_i.between?(1, Song.all.length)
         puts "Playing #{song.name} by #{song.artist.name}"
       end
     end
